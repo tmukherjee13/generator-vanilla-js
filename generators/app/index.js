22b6080e13065b040babdf9a7b1bae1d93bc865f
Tarun M <tarun.mukherjee@indusnet.co.in>
2017-02-09 13:25:37 +0530

    modified templates, included lodash, created jshint

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,8 +1,9 @@
-/* jslint node: true */
 'use strict';
 let Generator = require('yeoman-generator');
 let chalk = require('chalk');
 let yosay = require('yosay');
+let beautify = require('gulp-beautify');
+let _ = require('lodash-addons');
 module.exports = Generator.extend({
   prompting() {
     // Have Yeoman greet the user.
@@ -15,9 +16,26 @@ module.exports = Generator.extend({
     }, {
       type: 'input',
       name: 'url',
-      message: "Your project's url",
-      default: "http://localhost"
-    },{
+      message: 'Your project\'s url',
+      default: 'http://localhost'
+    }, {
+      type: 'checkbox',
+      name: 'features',
+      message: 'What more would you like?',
+      choices: [{
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      }, {
+        name: 'Sass',
+        value: 'includeSass',
+        checked: false
+      }, {
+        name: 'Modernizr',
+        value: 'includeModernizr',
+        checked: false
+      }]
+    }, {
       type: 'confirm',
       name: 'setup',
       message: 'Would you like proceed with the setup?',
@@ -27,36 +45,43 @@ module.exports = Generator.extend({
       .then(function (props) {
         // To access props later use this.props.someAnswer;
         this.props = props;
+        let hasFeature = feat => this.props.features && this.props.features.indexOf(feat) !== -1;
+        this.includeSass = hasFeature('includeSass');
+        this.includeBootstrap = hasFeature('includeBootstrap');
+        this.includeModernizr = hasFeature('includeModernizr');
       }.bind(this));
   },
   writing() {
+    // this.registerTransformStream(beautify({
+    //   indentSize: 2
+    // }));
+    // Copy the configuration files
     let config = function () {
-      // Copy the configuration files
+      this.fs.copy(this.templatePath('config/bowerrc'), this.destinationPath('.bowerrc'));
       this.fs.copyTpl(this.templatePath('config/_package.json'), this.destinationPath('package.json'), {
+        _: _,
         name: this.props.name
       });
       this.fs.copyTpl(this.templatePath('config/_bower.json'), this.destinationPath('bower.json'), {
-        name: this.props.name
+        _: _,
+        name: this.props.name,
+        includeBootstrap: this.includeBootstrap
       });
-      this.fs.copy(this.templatePath('config/bowerrc'), this.destinationPath('.bowerrc'));
-      this.fs.copyTpl(this.templatePath('config/_webpack.config.js'), this.destinationPath('webpack.config.js'), {});
-      this.fs.copyTpl(this.templatePath('public/index.html'), this.destinationPath('public/index.html'), {
-         name: this.props.name,
-         baseurl: this.props.url,
+      this.fs.copyTpl(this.templatePath('config/_webpack.config.js'), this.destinationPath('webpack.config.js'), {
+        _: _
       });
-      this.fs.copy(this.templatePath('public/assets'), this.destinationPath('public/assets'));
     };
-
+    // Copy application files
     let app = function () {
-      // Copy application files
       this.fs.copyTpl(this.templatePath('public/index.html'), this.destinationPath('public/index.html'), {
-         name: this.props.name,
-         baseurl: this.props.url,
+        _: _,
+        name: this.props.name,
+        baseurl: this.props.url
       });
       this.fs.copy(this.templatePath('public/assets'), this.destinationPath('public/assets'));
     };
-
-    
+    console.log(this.props);
+    // make calls to setup the project
     config.call(this);
     app.call(this);
   },
